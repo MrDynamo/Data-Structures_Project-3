75624fede2dc73cc13909a19c16b1926b4dc6ab5
Walker Morgan <mrdynamomlg@gmail.com>
2020-10-28 19:08:48 -0500

    Create game class for implementing GameTree interface

---
+++ src/com/github/mrdynamo/Project_3/Game.java
@@ -0,0 +1,59 @@
+package com.github.mrdynamo.Project_3;
+
+public class Game implements GameTree {
+
+    // Start game - implement
+    @Override
+    public void startGame(String question, String yesAnswer, String noAnswer) {
+
+    }
+
+    // New round - implement
+    @Override
+    public void newRound() {
+
+    }
+
+    // End game - implement
+    @Override
+    public void endGame() {
+
+    }
+
+    // Move yes - implement
+    @Override
+    public void moveYes() throws TreeException {
+
+    }
+
+    // Move no - implement
+    @Override
+    public void moveNo() throws TreeException {
+
+    }
+
+    // Get question - implement
+    @Override
+    public String getQuestion() throws TreeException {
+        return null;
+    }
+
+    // Set question - implement
+    @Override
+    public void setQuestion(String question, String answer) {
+
+    }
+
+    // Get answer - implement
+    @Override
+    public String getAnswer() throws TreeException {
+        return null;
+    }
+
+    // Is question? - implement
+    @Override
+    public boolean isQuestion() {
+        return false;
+    }
+
+}
---
+++ src/com/github/mrdynamo/Project_3/GameDriver.java
@@ -0,0 +1,10 @@
+package com.github.mrdynamo.Project_3;
+
+public class GameDriver {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
